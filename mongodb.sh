7d5667cf8174480604ae14f6a7e5b11665197926
BPR <bpr3834@gmail.com>
2023-01-25 00:34:14 +0530

    updating ./roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,21 @@
 source common.sh
 
 print_head "Copy MongoDB Repo File"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
 print_head "Install MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${LOG}
 status_check
 
 print_head "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 status_check
 
 print_head "Enable MongoDB"
-systemctl enable mongod
+systemctl enable mongod &>>${LOG}
 status_check
 
 print_head "Restart MongoDB"
-systemctl restart mongod
+systemctl restart mongod &>>${LOG}
 status_check
