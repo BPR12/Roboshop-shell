c8114325b4196f06bbf828b23b57f1799e9375ab
BPR <bpr3834@gmail.com>
2023-01-24 23:54:05 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -2,12 +2,12 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 status_check() {
 if [ $? -eq 0 ] ; then
-  echo -e "\e[1;32m SUCCESS\e[0m"
+  echo -e "\e[32m SUCCESS\e[0m"
 else
-  echo -e "\e[1;31m FAILURE\e[0m"
+  echo -e "\e[31m FAILURE\e[0m"
   exit
 fi
 }
 print_head() {
-echo -e "\e[1;35m $1\e[0m"
+echo -e "\e[35m $1\e[0m"
 }
