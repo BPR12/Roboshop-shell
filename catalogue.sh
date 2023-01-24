5858e6d2b630bb7e0ffb4e0d05ca8be88d869148
BPR <bpr3834@gmail.com>
2023-01-24 13:43:49 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Configuring NodeJs repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{LOG}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -11,7 +11,7 @@ else
   fi
 
 echo -e "\e[35m Install NodeJs\e[0m"
-yum install nodejs -y &>>{LOG}
+yum install nodejs -y &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -20,7 +20,7 @@ else
   fi
 
 echo -e "\e[35m Add Application user\e[0m"
-useradd roboshop &>>{LOG}
+useradd roboshop &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -28,10 +28,10 @@ else
   exit
   fi
 
-mkdir -p /app &>>{LOG}
+mkdir -p /app &>>${LOG}
 
 echo -e "\e[35m Downloading App Content\e[0m"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{LOG}
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -40,7 +40,7 @@ else
   fi
 
 echo -e "\e[35m Cleanup Old Content\e[0m"
-rm -rf /app/* &>>{LOG}
+rm -rf /app/* &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -51,7 +51,7 @@ else
 cd /app
 
 echo -e "\e[35m Extarcting App Content\e[0m"
-unzip /tmp/catalogue.zip &>>{LOG}
+unzip /tmp/catalogue.zip &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -62,7 +62,7 @@ else
 cd /app
 
 echo -e "\e[35m Installing NodeJs Dependencies\e[0m"
-npm install &>>{LOG}
+npm install &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -71,7 +71,7 @@ else
   fi
 
 echo -e "\e[35m Configuring Catalogue Service File\e[0m"
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>{LOG}
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -80,7 +80,7 @@ else
   fi
 
 echo -e "\e[35m Reload systemD\e[0m"
-systemctl daemon-reload &>>{LOG}
+systemctl daemon-reload &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -89,7 +89,7 @@ else
   fi
 
 echo -e "\e[35m enable Catalogue Service\e[0m"
-systemctl enable catalogue &>>{LOG}
+systemctl enable catalogue &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -98,7 +98,7 @@ else
   fi
 
 echo -e "\e[35m Start CatalogueService\e[0m"
-systemctl start catalogue &>>{LOG}
+systemctl start catalogue &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -107,7 +107,7 @@ else
   fi
 
 echo -e "\e[35m Configuring Mongo Repo\e[0m"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{LOG}
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -116,7 +116,7 @@ else
   fi
 
 echo -e "\e[35m Install Mongo Client\e[0m"
-yum install mongodb-org-shell -y &>>{LOG}
+yum install mongodb-org-shell -y &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
@@ -125,7 +125,7 @@ else
   fi
 
 echo -e "\e[35m Load Schema\e[0m"
-mongo --host mongodb-dev.devops22.online</app/schema/catalogue.js &>>{LOG}
+mongo --host mongodb-dev.devops22.online</app/schema/catalogue.js &>>${LOG}
 if [ $? -eq 0 ] ; then
   echo SUCCESS
 else
