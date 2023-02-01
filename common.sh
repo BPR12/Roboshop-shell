88af48c296b673965a72d86c925a084ec54f005c
BPR <bpr3834@gmail.com>
2023-02-01 12:52:02 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -68,7 +68,8 @@ NODEJS() {
   print_head "Start ${component} Service"
   systemctl start ${component} &>>${LOG}
   status_check
-if [ ${schema_load} == "true"]; then
+
+if [ ${schema_load} == "true" ]; then
   print_head "Configuring Mongo Repo"
   cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
   status_check
