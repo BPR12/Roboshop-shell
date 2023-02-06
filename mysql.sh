4c0b001f09652d92b6fd9695460aa65017176655
BPR <bpr3834@gmail.com>
2023-02-06 21:34:05 +0530

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,10 @@
 source common.sh
 
+if [ -z "${root_mysql_password}" ]; then
+  echo "variable root_mysql_password is missing"
+  exit
+fi
+
 print_head "Disable MySQL Default Module"
 dnf module disable mysql -y &>>${LOG}
 status_check
@@ -17,6 +22,11 @@ print_head "Enable MySQL"
 systemctl enable mysql &>>${LOG}
 status_check
 
-print_head "Restart MySQL"
+print_head "Start MySQL"
 systemctl restart mysql &>>${LOG}
 status_check
+
+print_head "Reset Default Database Password"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+status_check
+
