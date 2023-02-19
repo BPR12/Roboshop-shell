ca6c8f0c06cbe4bb071321e29abb63a5f1ef33d2
BPR <bpr3834@gmail.com>
2023-02-19 23:45:23 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -153,10 +153,9 @@ PYTHON() {
    status_check
 
    print_head "Update Passwords in Service File "
-   sed -i -e "s/roboshop_rabbitmq_password/${roboshop_rabbitmq_password}" {script_location}/files/${component} &>>${LOG}
+   sed -i -e "s/roboshop_rabbitmq_password/${roboshop_rabbitmq_password}" ${script_location}/files/${component} &>>${LOG}
    status_check
 
    SYSTEMD_SETUP
 
 }
- 
